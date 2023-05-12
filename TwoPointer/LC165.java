2a1b2116bc6643fbf086aff9afb1074494064eeb
derrick92 <derrick92@qq.com>
2023-05-12 19:48:01 +0800

    æ剔除前缀0

--- TwoPointer/LC165.java
+++ TwoPointer/LC165.java
@@ -3,6 +3,16 @@ package TwoPointer;
 public class LC165 {
     public static void main(String[] args) {
         System.out.println(compareVersion("1.00.1", "1.0.2"));
+
+        //从中学道了 字符串剔除前缀0的方法
+        // 方法1
+        String str = "0000123";
+        String newStr = str.replaceFirst("^0*", "");
+        System.out.println(newStr);
+        // 方法2
+        String str1 = "0000123";
+        String newStr1 = str.replaceAll("^(0+)", "");
+        System.out.println(newStr);
     }
 
     public static int compareVersion(String version1, String version2) {
