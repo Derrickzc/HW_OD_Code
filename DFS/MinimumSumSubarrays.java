a4754fca6a59eacb2dd1df7e6a2d4dd8feacabb2
derrick92 <578298450@qq.com>
2023-06-03 12:58:48 +0800

    add MinimumSumSubarrays problem description

--- DFS/MinimumSumSubarrays.java
+++ DFS/MinimumSumSubarrays.java
@@ -8,6 +8,29 @@ import java.util.List;
 /*
     等和子数组最小和
     与Lc 698相似
+
+
+    题目描述
+       给定一个数组nums，将元素分为若干个组，使得每组和相等，求出满足条件的所有分组中，组内元素和的最小值。
+
+输入描述
+    第一行输入 m
+    接着输入m个数，表示此数组nums
+    数据范围：1<=m<=50, 1<=nums[i]<=50
+
+输出描述
+    最小拆分数组和
+
+用例
+    输入	7
+    4 3 2 3 5 2 1
+    输出	5
+说明
+    可以等分的情况有：
+
+    4 个子集（5），（1,4），（2,3），（2,3）
+    2 个子集（5, 1, 4），（2,3, 2,3）
+    但最小的为5。
  */
 public class MinimumSumSubarrays {
     static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
