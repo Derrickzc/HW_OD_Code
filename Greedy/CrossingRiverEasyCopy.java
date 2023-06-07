2cdb09a18220ea2934a503528779b3926c9303f3
derrick92 <578298450@qq.com>
2023-06-07 20:41:27 +0800

    add more soft link

---
+++ Greedy/CrossingRiverEasyCopy.java
@@ -0,0 +1,53 @@
+package Greedy;
+
+import java.io.*;
+import java.util.Arrays;
+
+/*
+题目来源
+1700 -- Crossing River (poj.org)
+
+题目描述
+    有N个人想要过河，但是只有一艘船，并且这艘船最多只能搭载两个人。
+    现在需要你制定某种策略，花费最少的时间，让所有人渡河。
+    注：每个人的划船速度不同，当两个人划船时，过河时间取决于耗时最长的那个人。
+
+输入描述
+    第一行输入一个整数T（1 ≤ T ≤ 20），表示有T组测试用例。
+    接下来输入T组用例，每组用例有两行：
+    第一行输入一个整数N（N <= 1000），表示有N个人需要过河。
+    第二行输入N个整数，以空格隔开，每个整数代表一个人的过河时间。注：每个人的过河时间都不会超过100秒。
+输出描述
+    针对每组测试用例，输出一行，输出内容为：该组用例中所有人过河所需的最短时间。
+ */
+public class CrossingRiverEasyCopy {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+        int N = Integer.parseInt(reader.readLine());
+        while (count -- > 0) {
+            int[] array = Arrays.stream(reader.readLine().split("\\s+")).mapToInt(Integer::parseInt).sorted().toArray();
+            int[] dp = new int[N];
+            if (N == 1) {
+                writer.write(String.valueOf(array[0]));
+            } else if (N == 2) {
+                writer.write(String.valueOf(array[1]));
+            } else if (N == 3) {
+                writer.write(String.valueOf(array[1] + array[2]));
+            } else {
+                dp[0] = array[0];
+                dp[1] = array[1];
+                dp[2] = array[1] + array[2] + array[0];
+
+                for (int i = 3; i < N; i++) {
+                    dp[i] = Math.min(dp[i - 1] + array[0] + array[i], dp[i - 2] + array[0] + 2 * array[1] + array[i]);
+                }
+                writer.write(String.valueOf(dp[N - 1]));
+                writer.newLine();
+            }
+        }
+        writer.flush();
+    }
+}
---
+++ Greedy/CrossingRiverSoldierCopy.java
@@ -0,0 +1,56 @@
+package Greedy;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class CrossingRiverSoldierCopy {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(reader.readLine());
+        int T = Integer.parseInt(reader.readLine());
+        int[] time = Arrays.stream(reader.readLine().split("\\s+")).mapToInt(Integer::parseInt).sorted().toArray();
+        int[] dp = new int[N];
+//        dp[0] = time[0];
+//        dp[1] = time[1];
+//        dp[2] = time[1] + time[0] + time[2];
+        for (int i = 0; i < N; i++) {
+            if (i == 0) {
+                dp[0] = time[0];
+                if (dp[0] > T)
+                {
+                    writer.write("0 0");
+                    writer.flush();
+                    return;
+                }
+            } else if (i == 1) {
+                dp[1] = time[1];
+            } else {
+                dp[i] = Math.min(dp[i - 1] + time[0] + time[i], dp[i - 2] + time[0] + 2*time[1] + time[i]);
+            }
+            if (dp[i] > T) {
+                writer.write(i + " " + dp[i - 1]);
+                writer.flush();
+                return;
+            }
+        }
+
+        writer.write(N + " " + dp[N - 1]);
+        writer.flush();
+//        if (dp[N - 1] <= T) {
+//            writer.write(String.join(" ", String.valueOf(N) , String.valueOf(dp[N - 1])));
+//        } else {
+//            for (int i = 0; i < N; i++) {
+//                if (dp[i] > T) {
+//                    if (i == 0) {
+//                        writer.write(String.join(" ", String.valueOf(1) , String.valueOf(dp[i])));
+//                    } else {
+//                        writer.write(String.join(" ", String.valueOf(i) , String.valueOf(dp[i - 1])));
+//                    }
+//                    break;
+//                }
+//            }
+//        }
+
+    }
+}
