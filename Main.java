de99e2f255e466b136252ef7c0340d6e2f4f2f3a
= <578298450@qq.com>
2023-06-26 15:23:06 +0800

    rewrite Round321C

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -21,7 +20,6 @@ public class Main {
             hasCats[i] = in.nextInt();
             store[i] = new ArrayList<>();
         }
-
         for (int i = 1; i <= n - 1; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
@@ -29,15 +27,52 @@ public class Main {
             store[b].add(a);
         }
         vis[1] = true;
-
+        dfs(1, hasCats[1]);
+        System.out.println(ans);
     }
 
+/*    public static void dfs(int index, int catCount) {
+        ArrayList<Integer> integers = store[index];
+        boolean isLeaf = true;
+        for (Integer cur : integers) {
+            if (vis[cur]) continue;
+            int x = (catCount + 1) * hasCats[cur];
+            vis[cur] = true;
+            if (x <= m) {
+                dfs(cur, x);
+            }
+            isLeaf = false;
+        }
+        if (isLeaf) {
+            ans++;
+        }
+    }*/
 
+    public static void dfs(int curNode, int tolerateCat) {
+        boolean isLeaf = store[curNode].size() == 1;
+        for (Integer integer : store[curNode]) {
+            if (vis[integer]) continue;
+            // leaf Node cannot go to their parents Node.
+            int x = (tolerateCat + 1) * hasCats[integer];
+            vis[integer] = true;
+            if (x <= m) {
+                dfs(integer, x);
+            }
+            isLeaf = false;
+        }
+        if (isLeaf) {
+            ans++;
+        }
+/*        boolean isLeafNode = store[curNode].isEmpty();
+        Wrong: Because leaf Node must have parents; But parents will not arrive (from their son node)
+*/
+    }
 }
 
 /*
     456C - Boredom / 455A - Boredom
     C. Kefa and Park
+    Important: isLeafNode !!
 
 
 
